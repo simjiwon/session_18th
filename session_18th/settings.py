22dc687214c23ca0655f273395636e03b4eff51d
simjiwon <wldnjs0919@likelion.org>
2019-07-31 21:55:10 +0900

    wpqkffinal

--- session_18th/settings.py
+++ session_18th/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', ')z@%6gmjq51tg4rnx(d1*%8)g7d+7_
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
